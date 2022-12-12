1211df1ac1743533439daf35bf6e62b43af8164c
paras205 <parasojha205@gmail.com>
2022-12-12 21:21:24 +0545

    test

--- class.interface.ts
+++ class.interface.ts
@@ -4,6 +4,10 @@ interface Car {
   speedMethod: (speed: number) => void;
 }
 
+interface Car2 extends Car {
+  name: string;
+}
+
 class AutoMobile implements Car {
   brand: string;
   speed: number;
@@ -12,5 +16,13 @@ class AutoMobile implements Car {
   }
 }
 
+const autoMobile2: Car2 = {
+  brand: "autoMobile2",
+  speed: 10,
+  name: "string",
+  speedMethod(speed) {
+    console.log(speed);
+  },
+};
 const carObject = new AutoMobile();
 carObject.speedMethod(50);
